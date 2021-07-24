ce8019d72ecc1881880e87ef1c05d55392a3513b
cruiseomondi90@gmail.com <jamesomondi1418@gmail.com>
2021-07-24 13:16:58 +0300

    added FirebaseAuthUI and code to register Activity

--- app/src/main/java/com/example/autobot1/activities/RegisterPage.java
+++ app/src/main/java/com/example/autobot1/activities/RegisterPage.java
@@ -1,11 +1,24 @@
 package com.example.autobot1.activities;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.autobot1.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class RegisterPage extends AppCompatActivity {
 
@@ -14,5 +27,42 @@ public class RegisterPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
+        final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+                new FirebaseAuthUIActivityResultContract(),
+                new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                    @Override
+                    public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                        onSignInResults(result);
+                    }
+                }
+        );
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build()
+        );
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+    private void onSignInResults(FirebaseAuthUIAuthenticationResult result){
+        IdpResponse response = result.getIdpResponse();
+        if(result.getResultCode()==RESULT_OK){
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (response.isNewUser()){
+                createNewUserInDb();
+            }
+        }else {
+            Toast.makeText(this,"Something went wrong try again",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void createNewUserInDb(){
+        startActivity(new Intent(this,MapActivity.class));
     }
 }
