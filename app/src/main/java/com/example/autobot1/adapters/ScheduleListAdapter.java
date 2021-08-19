a9623e0bdadcd43ec15ffdbf293d5062da2864cd
CrazySkunk <cruiseomondi90@gmail.com>
2021-08-19 18:18:14 +0300

    New set up

---
+++ app/src/main/java/com/example/autobot1/adapters/ScheduleListAdapter.java
@@ -0,0 +1,23 @@
+package com.example.autobot1.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.autobot1.models.ScheduleItem;
+
+public class ScheduleListAdapter extends ArrayAdapter<ScheduleItem> {
+    public ScheduleListAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
